3389722c57b90d4139dc989dd1e94e48f3c5d267
Rael Kenny <RaelKenny@gmail.com>
2017-04-05 13:17:13 -0400

    initial commit

---
+++ Reinder/Streams/Pods/Target Support Files/DateTools/DateTools-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double DateToolsVersionNumber;
+FOUNDATION_EXPORT const unsigned char DateToolsVersionString[];
+
--- Streams/Streams/AppDelegate.swift
+++ Streams/Streams/AppDelegate.swift
@@ -2,49 +2,26 @@
 //  AppDelegate.swift
 //  Streams
 //
-//  Created by Reinder de Vries on 03-05-15.
-//  Copyright (c) 2015 LearnAppMaking. All rights reserved.
+//  Created by Rael Kenny on 4/5/17.
+//  Copyright © 2017 Rael Kenny. All rights reserved.
 //
 
 import UIKit
-import Parse
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate
-{
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
     var window: UIWindow?
-    
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool
-    {
-        let configuration = ParseClientConfiguration {
-            $0.applicationId = "..."
-            $0.clientKey = "..."
-            $0.server = "..."
-        }
-        
-        Parse.initialize(with: configuration);
-        
-        // Log out the user at every app start, i.e. log ins aren't remembered
-        PFUser.logOut();
-        
-        // Create a new StreamViewController for the current user and add it to the window + navigation controller
-        var streamVC:StreamViewController = StreamViewController();
-        streamVC.title = "Streams";
-        
-        var navigationVC:UINavigationController = UINavigationController(rootViewController: streamVC);
-        
-        let frame = UIScreen.main.bounds;
-        window = UIWindow(frame: frame);
-        
-        window!.rootViewController = navigationVC;
-        window!.makeKeyAndVisible();
-        
-        return true;
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
@@ -53,7 +30,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
